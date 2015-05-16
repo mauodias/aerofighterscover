bd29c844e6af8350c278ceb8b96ccabfe5dcf87f
mauodias <mau.dias@gmail.com>
2015-05-16 16:38:52 -0300

    Repositorio nao vai mais incluir o binario final

--- bin/PlayState.sh
+++ bin/PlayState.sh
@@ -1,1 +1,1 @@
-LD_LIBRARY_PATH=D_LIBRARY_PATH:.:/home/mauricio/test/lib:/home/mauricio/test/lib/Box2D-2.3.0/lib-Linux64:/home/mauricio/test/lib/SFML-2.2/lib-Linux64 /home/mauricio/test/bin/PlayState
+LD_LIBRARY_PATH=D_LIBRARY_PATH:.:/home/mauricio/aerofighterscover/lib:/home/mauricio/aerofighterscover/lib/Box2D-2.3.0/lib-Linux64:/home/mauricio/aerofighterscover/lib/SFML-2.2/lib-Linux64 /home/mauricio/aerofighterscover/bin/PlayState
